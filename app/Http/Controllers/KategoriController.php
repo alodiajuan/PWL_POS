5edd12cd1ad215c592d67a604ef7bb727e2ff305
alodiajuann <alodiajuan16@gmail.com>
2024-09-15 15:57:39 +0700

    Pertemuan3-Praktikum5

--- app/Http/Controllers/KategoriController.php
+++ app/Http/Controllers/KategoriController.php
@@ -9,12 +9,12 @@ class KategoriController extends Controller
 {
     public function index()
     {
-        $data = [
-            'kategori_kode' => "SNK",
-            'kategori_nama' => "Snack/Makanan Ringan",
-            'created_at' => now()
-        ];
-        DB::table('m_kategori')->insert($data);
-        return 'Insert data baru berhasil';
+        // $data = [
+        //     'kategori_kode' => "SNK",
+        //     'kategori_nama' => "Snack/Makanan Ringan",
+        //     'created_at' => now()
+        // ];
+        $row = DB::table('m_kategori')->where('kategori_kode', 'SNK')->delete();
+        return 'Delete data berhasil. Jumlah data yang dihapus: ' .$row. ' baris';
     }
 }
