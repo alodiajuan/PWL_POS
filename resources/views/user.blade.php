1e62c04786864974ef0557ba1d33f6669a4e197a
alodiajuann <alodiajuan16@gmail.com>
2024-09-20 16:52:43 +0700

    Pertemuan 4-Praktikum 2.6

--- resources/views/user.blade.php
+++ resources/views/user.blade.php
@@ -1,10 +1,6 @@
-<!DOCTYPE html>
-<html>
-    <head>  
-        <title>Data User</title>
-    </head>
     <body>
         <h1>Data User</h1>
+        <a href="{{ url('/user/tambah') }}">+ Tambah User</a>
         <table border="1" cellpadding="2" cellspacing="0">
             <tr>
                 <td>ID</td>
@@ -19,9 +15,8 @@
                 <td>{{$d->username }}</td>
                 <td>{{$d->nama }}</td>
                 <td>{{$d->level_id }}</td>
-                <td><a href="/user/ubah/{{$d->user_id}}>Ubah</a> | <a href = "/user/hapus{{ $d->user_id}}">Hapus</a></td>
+                <td><a href="{{ url('/user/ubah/' . $d->user_id) }}">Ubah</a> | <a href="{{url('/user/hapus/' .$d->user_id)}}">Hapus</a></td>
             </tr>
             @endforeach
         </table>
     </body>
-</html>
